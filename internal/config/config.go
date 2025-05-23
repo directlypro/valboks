295a40793bda4af8f051bac09953179ef06caf59
Providence <111455427+directlypro@users.noreply.github.com>
2025-05-23 22:40:26 +0200

    vbcli-232240

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,11 +1,13 @@
 package config
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
+	"path/filepath"
 )
 
-type Config string {
+type Config struct {
 	AppKey string `json:"app_key"`
 	AppSecret string `json:"app_secret"`
 	AccessToken string `json:"access_token"`
@@ -29,5 +31,58 @@ func NewConfigManager() (*ConfigManager, error) {
 	if err != nil {
 		return nil, fmt.Errorf("error creating config directory: %w", err)
 	}
+
+	configPath := filepath.Join(configDir, "config.json")
+
+	return &ConfigManager{
+		configPath: configPath,
+		config: &Config{},
+	}, nil
 }
 
+func (m *ConfigManager) Load() error {
+	if _, err := os.Stat(m.configPath); os.IsNotExist(err) {
+		return nil
+	}
+
+	data, err := os.ReadFile(m.configPath)
+	if err != nil {
+		return fmt.Errorf("error reading config file: %w", err)
+	}
+
+	err = json.Unmarshal(data, m.config)
+	if err != nil {
+		return fmt.Errorf("error parsing config fiel: %w", err)
+	}
+
+	return nil
+}
+
+func (m *ConfigManager) Save() error {
+	data, err := json.MarshalIndent(m.config, "", "  ")
+	if err != nil {
+		return fmt.Errorf("error serializing config: %w", err)
+	}
+
+	err = os.WriteFile(m.configPath, data, 0600)
+	if err != nil {
+		return fmt.Errorf("error writing config file: %w", err)
+	}
+
+	return nil
+}
+
+func (m *ConfigManager) SetCredentials(appKey, appSecret, accessToken string) {
+	m.config.AppKey = appKey
+	m.config.AppSecret = appSecret
+	m.config.AccessToken = accessToken
+}
+
+func (m *ConfigManager) SetTokens(accessToken, refreshToken string) {
+	m.config.AccessToken = accessToken
+	m.config.RefreshToken = refreshToken
+}
+
+func (m *ConfigManager) IsConfigured() bool {
+	return m.config.AccessToken != ""
+}
